caf53c4f2631bbce68421e6b339fbd8ad7de5a26
ValadaRP <79703007+ValadaRP@users.noreply.github.com>
2022-04-14 23:14:07 +0200

    First steps

--- meal-planning/src/App.js
+++ meal-planning/src/App.js
@@ -1,7 +1,34 @@
-import React from "react";
-
+import React, {useState} from "react";
+import MealList from "./MealList";
 function App() {
-    return <div className="App"></div>;
+    const [mealData, setMealData] = useState(null);
+    const [calories, setCalories] = useState(2000);
+
+    function handleChange(e){
+        setCalories(e.target.value);
+    }
+
+
+
+    function getMealData(){
+        fetch(`https://api.spoonacular.com/mealplanner/generate?apiKey=ec32e4c166da492293a046644d6b71e1&timeFrame=day&targetCalories=${calories}`)
+            .then(res => res.json())
+            .then(data => {
+                setMealData(data);
+                console.log(data);
+            })
+            .catch(() =>{
+                console.log("error");
+            })
+    }
+
+    return <div className="App">
+        <section className="controls">
+            <input type="numer" placeholder="Calories" onChange={handleChange}/>
+        </section>
+        <button onClick={getMealData}>Sprawdz dzienny plan!</button>
+        {mealData && <MealList mealList={mealData}/>}
+    </div>;
 }
 
 export default App;
